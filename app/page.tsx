d7008adf40bb082af3d9dd044e868d701a9e0359
smokeking80 <76210354+smokeking80@users.noreply.github.com>
2026-09-13 02:12:00 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -385,7 +385,7 @@ function ProductItem({
       <button
         className="secondary"
         onClick={togglePriceWatch}
-        disabled={watchingLoading}
+        disabled={false}
         title={
           item.watch_price
             ? "Zrušit hlídání ceny"
