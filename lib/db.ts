ec705b353a6c8fa22cd04f91a7075d6a4ad2b033
smokeking80 <76210354+smokeking80@users.noreply.github.com>
2026-09-16 13:50:07 +0200

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -63,6 +63,11 @@ await sql`
   ALTER TABLE found_items
   ADD COLUMN IF NOT EXISTS target_price INTEGER;
 `;
+  
+   await sql`
+   ALTER TABLE found_items
+   ADD COLUMN IF NOT EXISTS target_reached_at TIMESTAMPTZ;
+  `;
 
   await sql`
   CREATE TABLE IF NOT EXISTS deleted_items (
